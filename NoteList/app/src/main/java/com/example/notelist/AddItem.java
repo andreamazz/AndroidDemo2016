c8530365369231f742a03311af066bc9328f79d7
tommasoberlose <tommaso.berlose@gmail.com>
2016-11-16 17:28:28 +0100

    Add New Item Activity

---
+++ NoteList/app/src/main/java/com/example/notelist/AddItem.java
@@ -0,0 +1,37 @@
+package com.example.notelist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Show back icon
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final EditText input = (EditText) findViewById(R.id.item_text);
+
+        Button actionSave = (Button) findViewById(R.id.action_save);
+        actionSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("NoteList", "Input: " + input.getText().toString());
+            }
+        });
+    }
+
+}
--- NoteList/app/src/main/java/com/example/notelist/Main.java
+++ NoteList/app/src/main/java/com/example/notelist/Main.java
@@ -1,10 +1,12 @@
 package com.example.notelist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -28,8 +30,8 @@ public class Main extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(Main.this, AddItem.class);
+                startActivity(i);
             }
         });
 
@@ -48,7 +50,7 @@ public class Main extends AppCompatActivity {
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_1,
-                mItems );
+                mItems);
 
         listView.setAdapter(arrayAdapter);
 
